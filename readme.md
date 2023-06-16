cdfe8f59a3d53eb863ae1b7d8bf2a80e9be5e7b4
Sander van Dragt <sander@vandragt.com>
2023-06-16 16:07:13 +0100

    init

---
+++ readme.md
@@ -0,0 +1,9 @@
+Bundle:
+
+```
+pyinstaller --onefile app.py && dist/app serve
+```
+
+# Conclusion
+
+Apps are 117mb zipped! slow to load.
---
+++ src/app.py
@@ -0,0 +1,25 @@
+import click
+import os
+import webview
+import sys
+from flask import Flask
+
+if getattr(sys, 'frozen', False):
+    template_folder = os.path.join(sys._MEIPASS, 'templates')
+    static_folder = os.path.join(sys._MEIPASS, 'static')
+    app = Flask(__name__, template_folder=template_folder, static_folder=static_folder)
+else:
+    app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.cli.command("serve")
+def serve():
+    webview.create_window('Hello world', app)
+    webview.start()
+
+if __name__ == "__main__":
+    serve()
