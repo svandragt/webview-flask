978f993443a74b1a653f2165adab6b09286f294c
Sander van Dragt <sander@vandragt.com>
2023-06-22 13:52:57 +0100

    Run Python 3.8 and up.

--- Makefile
+++ Makefile
@@ -2,13 +2,12 @@ bootstrap:
 	rm -rf dist/
 	poetry install
 	poetry export -o requirements.txt --without-hashes
+	poetry run black "src/webview_flask/"
 
 build: bootstrap
-	poetry run pex --python=$(OUTPUT) -v . -r requirements.txt -o dist/webview_flask.pex -e webview_flask.main:main
-	cd dist
-	unzip webview_flask.pex
-	zip -r webview_flask.pex.zip webview_flask.pex
-	cd ..
+	poetry run pex -v . -r requirements.txt -o dist/webview_flask.pex -e webview_flask.main:main --python-shebang="/usr/bin/env python3"
+	unzip dist/webview_flask.pex -d dist/
+	zip -jr dist/webview_flask.pex.zip dist/webview_flask.pex
 
-build-run: build
+run: build
 	dist/webview_flask.pex
--- src/webview_flask/main.py
+++ src/webview_flask/main.py
@@ -4,12 +4,13 @@ import webview
 import sys
 from flask import Flask
 
-if getattr(sys, 'frozen', False):
-    template_folder = os.path.join(sys._MEIPASS, 'templates')
-    static_folder = os.path.join(sys._MEIPASS, 'static')
+if getattr(sys, "frozen", False):
+    template_folder = os.path.join(sys._MEIPASS, "templates")
+    static_folder = os.path.join(sys._MEIPASS, "static")
     app = Flask(__name__, template_folder=template_folder, static_folder=static_folder)
 else:
     app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
@@ -17,8 +18,13 @@ def hello_world():
 
 
 def main():
-    webview.create_window('Hello world', app)
+    if not (sys.version_info >= (3, 8) and sys.version_info < (3, 12)):
+        print("Python version is not between 3.8 and 3.11.")
+        sys.exit(1)
+
+    webview.create_window("Hello world", app)
     webview.start()
+
 
 if __name__ == "__main__":
     main()
